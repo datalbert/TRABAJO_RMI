a68f5bd1d8de9d045f54a9c52003c8c8cf034cc8
Ismael García Mayorga <118350831+ismaelgmy@users.noreply.github.com>
2023-04-13 11:58:53 +0200

    Add files via upload

---
+++ inventarioBD/inventarioInsert.sql
@@ -0,0 +1,94 @@
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (100, '0x433' ,'Nike','S','negro','camiseta', 'regular fit', '10');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (100, '0x432' ,'Nike','S','azul','camiseta', 'regular fit', '7');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (100, '0x431' ,'Nike','M','negro','camiseta', 'regular fit', '10');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (100, '0x435' ,'Nike','M','rojo','camiseta', 'regular fit', '12');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (100, '0x423' ,'Nike','L','negro','camiseta', 'regular fit', '4');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (100, '0x438' ,'Nike','L','blanco','camiseta', 'regular fit', '3');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (200, '0x533' ,'Puma','S','negro','sudadera', 'Oversize', '4');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (200, '0x534' ,'Puma','M','negro','sudadera', 'Oversize', '7');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (200, '0x535' ,'Puma','L','negro','sudadera', 'Oversize', '10');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (200, '0x536' ,'Puma','S','azul','sudadera', 'Oversize', '10');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (200, '0x537' ,'Puma','M','azul','sudadera', 'Oversize', '12');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (200, '0x538' ,'Puma','L','azul','sudadera', 'Oversize', '8');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (200, '0x539' ,'Puma','S','rojo','sudadera', 'Oversize', '6');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (200, '0x540' ,'Puma','M','rojo','sudadera', 'Oversize', '9');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (200, '0x541' ,'Puma','L','rojo','sudadera', 'Oversize', '5');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (322, '0x123' ,'converse','42','negro','deportes', '70s', '5');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (322, '0x544' ,'converse','42','blanco','deportes', '70s', '5');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (322, '0x987' ,'converse','43','negro','deportes', '70s', '9');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (322, '0x345' ,'converse','43','blanco','deportes', '70s', '10');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (455, '0x633' ,'adidas','43','blanco','deportes', 'gasele', '5');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (455, '0x623' ,'adidas','42','blanco','deportes', 'gasele', '7');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (455, '0x663' ,'adidas','43','negro','deportes', 'gasele', '10');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x693' ,'Levis','S','azul','pantalon', 'Vaquero', '4');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x634' ,'Levis','M','azul','pantalon', 'Vaquero', '7');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x635' ,'Levis','L','azul','pantalon', 'Vaquero', '9');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x636' ,'Levis','S','negro','pantalon', 'Vaquero', '2');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x637' ,'Levis','M','negro','pantalon', 'Vaquero', '8');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x638' ,'Levis','L','negro','pantalon', 'Vaquero', '23');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x639' ,'Levis','S','burdeos','pantalon', 'Vaquero', '7');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x640' ,'Levis','M','burdeos','pantalon', 'Vaquero', '11');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x641' ,'Levis','L','burdeos','pantalon', 'Vaquero', '12');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x642' ,'Levis','S','gris','pantalon', 'Vaquero', '3');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x643' ,'Levis','M','gris','pantalon', 'Vaquero', '4');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (566, '0x644' ,'Levis','L','gris','pantalon', 'Vaquero', '5');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (822, '0x899' ,'Fila','M','blanco','calcetines', 'high lenght', '5');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (822, '0x988' ,'Fila','L','blanco','calcetines', 'high lenght', '6');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (822, '0x155' ,'Fila','M','negro','calcetines', 'high lenght', '3');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (822, '0x166' ,'Fila','L','negro','calcetines', 'high lenght', '4');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (709, '0x733' ,'Calvin Klein','S','azul','calzoncillos', 'Boxer', '23');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (709, '0x734' ,'Calvin Klein','M','azul','calzoncillos', 'Boxer', '13');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (709, '0x735' ,'Calvin Klein','L','azul','calzoncillos', 'Boxer', '15');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (709, '0x736' ,'Calvin Klein','S','negro','calzoncillos', 'Boxer', '18');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (709, '0x737' ,'Calvin Klein','M','negro','calzoncillos', 'Boxer', '19');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (709, '0x738' ,'Calvin Klein','L','azul','calzoncillos', 'Boxer', '25');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (710, '0x739' ,'Calvin Klein','S','rojo','calzoncillos', 'Slip', '20');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (710, '0x740' ,'Calvin Klein','M','rojo','calzoncillos', 'Slip', '28');
+INSERT INTO INVENTARIO (id, codigo_barras , marca, talla, color, tipo, modelo, cantidad) VALUES
+            (710, '0x741' ,'Calvin Klein','L','rojo','calzoncillos', 'Slip', '15');
